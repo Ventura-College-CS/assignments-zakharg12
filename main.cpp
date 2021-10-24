7b540451b6db56eeffbffbfb3d07d36d1af76dca
Zakhar Gazizov <druzelol57@gmail.com>
2021-10-24 21:34:06 +0000

    2-2.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+void printArray(double * const n,int N);
+
+int main()
+{
+    const int N = 10;
+    double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5, 13.8, 14.7, 19.8, 20.0};
+
+    printArray(numbers, N);
+}
+
+void printArray(double *ptr, int N)
+{
+    for(int i=0; i<10; i++)
+    {
+        cout << *(ptr+i) << "\t";
+    }
+    cout << endl;
 }
